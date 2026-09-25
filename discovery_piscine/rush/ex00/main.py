c15f85b6698a7df47770d8365c1661ebe51a74c0
Tanawat235 <67070235@kmitl.ac.th>
2026-09-25 23:32:49 +0700

    Update main.py

--- discovery_piscine/rush/ex00/main.py
+++ discovery_piscine/rush/ex00/main.py
@@ -1,1 +1,15 @@
+from checkmate import checkmate
 
+def main():
+    # ตัวอย่างกระดานขนาด 4x4
+    # ในตัวอย่างนี้ R อยู่แนวเดียวกับ K ทางขวา จึงเล็งถึง K
+    board = """\
+....
+.K.. 
+..P. 
+....\
+"""
+    checkmate(board)
+
+if __name__ == "__main__":
+    main()
